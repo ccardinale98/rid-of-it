5a5f88755283d0a7faab724f52952be27ee1bb19
ccardinale98 <ccardinale98@gmail.com>
2021-10-24 19:35:26 -0400

    added home page

--- client/App.js
+++ client/App.js
@@ -1,31 +1,79 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View, SafeAreaView, Button } from "react-native";
+
+const x = 1;
 
 export default function App() {
-  function checkLogged() {
-    const response = fetch(`localhost:3001/api/registration/`, {
-      method: "GET",
-    });
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+  console.log(data);
 
-    return response;
+  useEffect(() => {
+    fetch("https://rid-of-it.herokuapp.com/api/registration/")
+      .then((response) => response.json())
+      .then((data) => setData(data))
+      .catch((err) => console.log(err))
+      .finally(() => setLoading(false));
+  }, []);
+
+  console.log(data);
+  if (data.user == "none") {
+    return (
+      <SafeAreaView style={styles.container}>
+        <Text style={styles.title}>Rid of It</Text>
+        <View style={styles.buttons}>
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>Login</Text>
+          </View>
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>Signup</Text>
+          </View>
+        </View>
+        <StatusBar style="auto" />
+      </SafeAreaView>
+    );
+  } else {
+    return (
+      <SafeAreaView style={styles.container}>
+        <Text>A user is logged in.</Text>
+        <StatusBar style="auto" />
+      </SafeAreaView>
+    );
   }
-
-  console.log(checkLogged());
-
-  return (
-    <SafeAreaView style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </SafeAreaView>
-  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#CAEBF2",
     alignItems: "center",
     justifyContent: "center",
+  },
+  title: {
+    flex: 1,
+    fontSize: 60,
+    color: "#A9A9A9",
+    paddingTop: 50,
+  },
+  buttons: {
+    flex: 10,
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+  },
+  button: {
+    width: "70%",
+    height: "17%",
+    borderRadius: 25,
+    backgroundColor: "#FF3B3F",
+    color: "#FF3B3F",
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom: 20,
+  },
+  buttonText: {
+    color: "#FFF",
+    fontSize: 25,
   },
 });
