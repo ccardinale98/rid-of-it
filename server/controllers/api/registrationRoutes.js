a08cbe14ad3ab76d2622d86a9ae9869da2b081b9
ccardinale98 <ccardinale98@gmail.com>
2021-10-24 15:54:28 -0400

    signup route

--- server/controllers/api/registrationRoutes.js
+++ server/controllers/api/registrationRoutes.js
@@ -33,3 +33,27 @@ router.post("/login", async (req, res) => {
     res.status(404).end();
   }
 });
+
+router.post("/signup", async (req, res) => {
+  console.log("POST /api/registration/signup");
+
+  try {
+    const user = await User.create({
+      user_name: req.body.user_name,
+      email: req.body.email,
+      password: req.body.password,
+      image: req.body.image,
+    });
+
+    req.session.save(() => {
+      req.session.user_id = user.id;
+      req.session.logged_in = true;
+
+      res.status(200).json(user);
+    });
+  } catch (err) {
+    console.log(err);
+
+    res.status(400).json(err);
+  }
+});
