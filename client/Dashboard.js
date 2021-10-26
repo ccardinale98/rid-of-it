02ac27f8c17bf67162481e0fea8cdd4abb856b77
ccardinale98 <ccardinale98@gmail.com>
2021-10-25 20:27:57 -0400

    update'

--- client/Dashboard.js
+++ client/Dashboard.js
@@ -11,12 +11,26 @@ import {
   FlatList,
   Image,
 } from "react-native";
+import { Icon } from "react-native-elements";
 import Home from "./Home";
 
 export default function Dashboard({ navigation }) {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
   const [current, getCurrent] = useState();
+
+  function logout() {
+    fetch("https://rid-of-it.herokuapp.com/api/registration/logout", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .catch((err) => console.log(err));
+    navigation.navigate("Home");
+  }
 
   function currentUser() {
     fetch("https://rid-of-it.herokuapp.com/api/registration/")
@@ -55,32 +69,110 @@ export default function Dashboard({ navigation }) {
     console.log(item.name);
     if (item.user_id != current) {
       return (
-        <View>
-          <Image
-            source={{ uri: item.image }}
-            style={{ width: 50, height: 50 }}
-          />
-          <Text>{item.name}</Text>
-          <Text>{item.description}</Text>
-          <Text>${item.price}</Text>
+        <View style={styles.boxDiv}>
+          <View style={styles.box}>
+            <Image
+              source={{ uri: item.image }}
+              style={{ width: 50, height: 50 }}
+              style={styles.image}
+            />
+            <View style={styles.details}>
+              <Text>{item.name}</Text>
+              <Text>${item.price}</Text>
+              <Button title="View" />
+            </View>
+          </View>
         </View>
       );
     }
   }
 
   return (
-    <SafeAreaView>
-      <View>
-        <View>
-          <Text>Dashboard</Text>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.nav}>
+        <View style={styles.profile}>
+          <Icon name="user" type="font-awesome" size="60" />
         </View>
-        <View></View>
-        <FlatList
-          data={data}
-          renderItem={renderPosts}
-          keyExtractor={(item) => item.id.toString()}
-        />
+        <View style={styles.post}>
+          <Icon name="plus-circle" type="font-awesome" size="60" />
+        </View>
+        <View style={styles.logout}>
+          <Icon
+            name="sign-out"
+            type="font-awesome"
+            size="60"
+            onPress={() => logout()}
+          />
+        </View>
       </View>
+      <View></View>
+      <FlatList
+        data={data}
+        renderItem={renderPosts}
+        keyExtractor={(item) => item.id.toString()}
+        style={styles.list}
+      />
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#CAEBF2",
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+  },
+  nav: {
+    width: "100%",
+    height: 70,
+    marginBottom: 20,
+    borderStyle: "solid",
+    borderBottomWidth: 5,
+    borderBottomColor: "#FF3B3F",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  list: {
+    width: "100%",
+  },
+  boxDiv: {
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  box: {
+    width: "80%",
+    backgroundColor: "#EFEFEF",
+    padding: 20,
+    borderColor: "#A9A9A9",
+    borderStyle: "solid",
+    borderWidth: 5,
+    flexDirection: "row",
+  },
+  image: {
+    flex: 2,
+  },
+  details: {
+    flex: 4,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  profile: {
+    height: "100%",
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  post: {
+    height: "100%",
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  logout: {
+    height: "100%",
+    marginLeft: 10,
+    marginRight: 10,
+  },
+});
